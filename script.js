5036b0ea9b8bfb5276596a64860644d329330d38
samrodd <loughlinrodd@gmail.com>
2021-10-06 11:02:29 -0400

    1st Commit for Remove-Column Adds remove column functionality to script.js

--- script.js
+++ script.js
@@ -11,41 +11,40 @@ function addR() {
         var row = tbl.insertRow();
         var cell = row.insertCell();
         numRows++;
+        numCols++;
     }
     //handle case where a column has been added
-    if(numCols>0){
+    else{
         var row = tbl.insertRow();
-        for(let i = 0; i <= numCols; i++){
+        for(let i = 0; i < numCols; i++){
             row.insertCell();
         }
-        numRows++;
-        
+        numRows++;    
     }
 
 }
 //Add a column
 function addC() {
-    //declare tbl variable and set it equal to grid element
-    var tbl = document.getElementById("grid");
-    //use querySelectorAll to get all tr elements in grid
-    const rows = tbl.querySelectorAll("tr");
-
-    //handle case where numRows === 0
-    if(numRows === 0){
-        var row = tbl.insertRow();
-        var cell = row.insertCell();
-        numRows++;
-    }
-
-    //handle all other cases
-    else{
-        for(let i = 0; i < rows.length; i++){
-            rows[i].insertCell();
-        }
-        numCols++; 
-
-    }
-
+     //declare tbl variable and set it equal to grid element
+     var tbl = document.getElementById("grid");
+     //use querySelectorAll to get all tr elements in grid
+     const rows = tbl.querySelectorAll("tr");
+ 
+     //handle case where numRows === 0
+     if(numRows === 0){
+         var row = tbl.insertRow();
+         var cell = row.insertCell();
+         numRows++;
+     }
+ 
+     //handle all other cases
+     else{
+         for(let i = 0; i < rows.length; i++){
+             rows[i].insertCell();
+         }
+         numCols++; 
+ 
+     }
 }
 
 //Remove a row
@@ -68,12 +67,32 @@ function removeR() {
 function removeC() {
     //declare tbl variable and set it equal to grid element
     var tbl = document.getElementById("grid");
+    //use querySelectorAll to get all tr elements in grid
     const rows = tbl.querySelectorAll("tr");
-    //tbl
-    for(let i = 0; i < numRows; i++){
-        rows[numRows-1].deleteCell[i];
+    if(numCols === 0){
+        numRows = 0;
+        console.log(numRows);
+        console.log(numCols);
+        return;
+
     }
-    numCols--;
+
+    else{
+        for(let i = 0; i < numRows; i++){
+            var r = tbl.rows[i];
+            r.deleteCell(0);
+        }
+        numCols--;
+        if(numCols === 0){
+            clearAll();
+            return;
+        }
+    }
+    console.log(numRows);
+    console.log(numCols);
+
+  //  console.log(numRows);
+
 
 }
 //sets global var for selected color
@@ -87,7 +106,14 @@ function fill(){
 }
 
 function clearAll(){
-    alert("Clicked Clear All")
+    //declare tbl variable and set it equal to grid element
+    var tbl = document.getElementById("grid");
+    
+    while(tbl.hasChildNodes()){
+        tbl.removeChild(tbl.firstChild);
+    }
+    numRows=0;
+    numCols=0;
 }
 
 function fillU(){
